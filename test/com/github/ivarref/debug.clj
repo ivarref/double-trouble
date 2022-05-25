cbf69d8e325ab2893cb0539827d9a090ed88909e
Ivar Refsdal <refsdal.ivar@gmail.com>
2022-05-25 11:20:18 +0200

    Start add build.edn

--- test/com/github/ivarref/debug.clj
+++ test/com/github/ivarref/debug.clj
@@ -3,9 +3,17 @@
 (ns clojure.core)
 
 (defn pp [x]
-  "like let, but binds the expressions globally."
   (clojure.pprint/pprint x)
   x)
 
 (defn b []
   (println "************************************"))
+
+(defmacro def-let
+  "like let, but binds the expressions globally."
+  [bindings & more]
+  (let [let-expr (macroexpand `(let ~bindings))
+        names-values (partition 2 (second let-expr))
+        defs   (map #(cons 'def %) names-values)]
+    (concat (list 'do) defs more)))
+
